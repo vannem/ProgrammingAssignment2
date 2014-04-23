ec5cbf76b4e8b41199f9947072d75645c7a6ab02
vannem <vannem.han@gmail.com>
2014-04-24 07:46:45 +0800

    update to add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this function gives you the inverse matrix of 
+## a invertible matrix you input. 
 
-## Write a short comment describing this function
-
+## Its input is a matrix, output is a list containing
+## 4 members. This function generally let you input 
+## the matrix, or get your inputted matrix, calculate the 
+## inverse matrix, or get your calculated inversed matrix.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -17,7 +19,10 @@ list(set = set, get = get,
      getInverse = getInverse)
 }
 
-## Write a short comment describing this function
+## If you already input the matrix and calculated
+## the inverse matrix, it will give out the results
+## from cache. If not, it will calculate them out 
+## and output them.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getInverse()
